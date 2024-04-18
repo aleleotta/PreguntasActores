55514ffda3af16a14a6d2a408d00cab6ab73a279
aleleotta <114069815+aleleotta@users.noreply.github.com>
2024-04-19 01:22:47 +0200

    .

--- Entities/clsActor.cs
+++ Entities/clsActor.cs
@@ -3,12 +3,40 @@
     public class clsActor
     {
         #region Attributes
+        private readonly int id = 0;
+        private string nombre = "";
+        private string apellidos = "";
+        private DateTime fechaNac;
+        private string nacionalidad = "";
+        private string foto = "";
         #endregion
 
         #region Properties
+        public int Id { get { return id; } }
+        public string Nombre { get {  return nombre; } }
+        public string Apellidos { get {  return apellidos; } }
+        public DateTime FechaNac { get {  return fechaNac; } }
+        public string Nacionalidad { get { return nacionalidad; } }
+        public string Foto { get { return foto; } }
         #endregion
 
         #region Constructors
+        public clsActor() { }
+        public clsActor(int id, string nombre, string apellidos, DateTime fechaNac, string nacionalidad)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+            this.nacionalidad = nacionalidad;
+        }
+        public clsActor(int id, string nombre, string apellidos, DateTime fechaNac, string nacionalidad, string foto)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+            this.nacionalidad = nacionalidad;
+            this.foto = foto;
+        }
         #endregion
     }
 }
--- Entities/clsRespuesta.cs
+++ Entities/clsRespuesta.cs
@@ -9,12 +9,25 @@ namespace Entities
     internal class clsRespuesta
     {
         #region Attributes
+        private readonly int id = 0;
+        private string frase = "";
+        private int idPregunta = 0;
         #endregion
 
         #region Properties
+        public int Id { get { return id; } }
+        public string Frase { get {  return frase; } }
+        public int IdPregunta { get {  return idPregunta; } }
         #endregion
 
         #region Constructors
+        public clsRespuesta() { }
+        public clsRespuesta(int id, string frase, int idPregunta)
+        {
+            this.id = id;
+            this.frase = frase;
+            this.idPregunta = idPregunta;
+        }
         #endregion
     }
 }
