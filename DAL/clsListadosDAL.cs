4cf991f732e581cd850c96194f2bfef72ae63d90
aleleotta <114069815+aleleotta@users.noreply.github.com>
2024-04-22 00:35:42 +0200

    .

--- DAL/clsListadosDAL.cs
+++ DAL/clsListadosDAL.cs
@@ -15,37 +15,28 @@ namespace DAL
                 new clsActor(),
                 new clsActor(),
                 new clsActor(),
+                new clsActor(),
+                new clsActor(),
+                new clsActor(),
+                new clsActor(),
+                new clsActor(),
+                new clsActor(),
+                new clsActor(),
+                new clsActor(),
+                new clsActor(),
+                new clsActor(),
+                new clsActor(),
+                new clsActor(),
+                new clsActor(),
+                new clsActor(),
                 new clsActor()
             };
             return listadoActores;
         }
-        public static List<clsPregunta> getListadoCompletoPreguntas()
-        {
-            List<clsPregunta> listadoPreguntas = new List<clsPregunta>
-            {
-                new clsPregunta()
-            };
-            return listadoPreguntas;
-        }
-        public static List<clsRespuesta> getListadoCompletoRespuestas()
-        {
-            List<clsRespuesta> listadoRespuestas = new List<clsRespuesta>
-            {
-                new clsRespuesta()
-            };
-            return listadoRespuestas;
-        }
+        
         public static clsActor getActorPorId(int idActor)
         {
             return new clsActor();
-        }
-        public static clsPregunta getPreguntaPorId(int idPregunta)
-        {
-            return new clsPregunta();
-        }
-        public static clsRespuesta getRespuestaPorId(int idRespuesta)
-        {
-            return new clsRespuesta();
         }
     }
 }
--- PreguntasActores/ViewModels/MainVM.cs
+++ PreguntasActores/ViewModels/MainVM.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using DAL;
 using Entities;
 using PreguntasActores.Utilities;
@@ -17,9 +12,13 @@ namespace PreguntasActores.ViewModels
         #region Attributes
         private ObservableCollection<clsActor> listadoActores;
         private ObservableCollection<clsActor> listadoCuatroActores;
-        private clsActor actorActual;
+        private clsActor actorActual; // El actor a adivinar.
         private clsActor actorSeleccionado;
+        private int aciertos = 0;
+        private DelegateCommand jugar;
         private DelegateCommand seleccionarRespuesta;
+        private DelegateCommand confirmar;
+        private DelegateCommand salir;
         #endregion
 
         #region Properties
@@ -37,12 +36,24 @@ namespace PreguntasActores.ViewModels
                 OnPropertyChanged("ActorSeleccionado");
             }
         }
+        public int Aciertos { get { return aciertos; } }
+        public DelegateCommand Jugar { get { return jugar; } }
+        public DelegateCommand SeleccionarRespuesta { get { return seleccionarRespuesta; } }
+        public DelegateCommand Confirmar { get { return confirmar; } }
+        public DelegateCommand Salir { get { return salir; } }
+        #endregion
+
+        #region CommandExecuters
+        private async void jugar_execute() => await Shell.Current.GoToAsync("///Partido");
+        private void salir_execute() => System.Environment.Exit(0);
         #endregion
 
         #region Constructor
         public MainVM()
         {
             listadoActores = new ObservableCollection<clsActor>(clsListadosDAL.getListadoCompletoActores());
+            jugar = new DelegateCommand(jugar_execute);
+            salir = new DelegateCommand(salir_execute);
         }
         #endregion
 
